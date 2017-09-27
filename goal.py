47c0d0b1bad994760ab1289cc6e61ca8563c64ba
IraEmi <ira.petrivska@gmail.com>
2017-09-27 17:36:55 +0300

    мета дописана

--- goal.py
+++ goal.py
@@ -4,3 +4,22 @@ goal = [ nombers, strong ]
 print ( goal )
 strong.append( 'i\'m  strong' )
 print ( goal )
+del nombers [0]
+print ( nombers + strong )
+i_can = nombers + strong
+print ( i_can )
+print ( nombers * 3 )
+nombers_2 = ( 1, 2 )
+print ( nombers_2 )
+
+
+favorite_dish = {'Sofiya' : 'sushi' ,
+                 'Emi' : 'borsch' ,
+                 'father' : 'beer' ,
+                 'mother' : 'eggs' }
+print ( favorite_dish )
+print ( favorite_dish [ 'father' ])
+del  favorite_dish [ 'mother' ]
+print ( favorite_dish )
+favorite_dish [ 'father'] = ' fish '
+print ( favorite_dish )
